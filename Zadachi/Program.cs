e5aad433e3422d5ad4ca2b7bcf0db100efac4a12
Aleksandr Korshunov <akor1191@mail.ru>
2023-12-09 18:50:55 +0300

    Added Zadacha 1

--- Zadachi/Program.cs
+++ Zadachi/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100.
+// Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+
+// int[] array = new int[10];
+// int kolElement = 0;
+
+// for(int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(1,101);  Z
+//     Console.Write(array[i]+ " ");
+// }
+// for(int i = 0; i < array.Length; i++)
+// {
+//     if(array[i] > 19 && array[i] < 91)
+//     {
+//         kolElement = kolElement +1;
+//     }
+// }
+// Console.WriteLine("Количество элементов, в отрезке [20, 90] = " +kolElement);
